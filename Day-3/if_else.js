61d49e185c492c765967f69d41ca43b8bcd7fea1
Prince Bansal <prince.bansal008@gmail.com>
2024-07-15 20:50:20 +0530

    if-else added

---
+++ Day-3/if_else.js
@@ -0,0 +1,15 @@
+const num = 69;
+if (num > 0) {
+    console.log("+ve");
+} else if (num < 0) {
+    console.log("-ve");
+} else {
+    console.log("0");
+}
+
+let age = 1;
+if (age >= 18) {
+    console.log("Yep, voting eligible");
+} else {
+    console.log(`Nope, you will be eligible in ${18 - age} years`);
+}
---
+++ Day-3/nested_if_else.js
@@ -0,0 +1,17 @@
+const a = 30;
+const b = 28;
+const c = 10;
+
+if (a > b) {
+    if (a > c) {
+        console.log("a is greatest");
+    } else {
+        console.log("c is greatest");
+    }
+} else {
+    if (b > c) {
+        console.log("b is greatest");
+    } else {
+        console.log("c is greatest");
+    }
+}
